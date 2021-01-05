626562a508131fea91710b2bba8b978e8cb94087
mohua12 <76768623+mohua12@users.noreply.github.com>
2021-01-05 15:39:55 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,34 +3,72 @@
   .controller('ToBuyController',ToBuyController)
 .controller('AlreadyBoughtController',AlreadyBoughtController)
 .service('ShoppingListCheckOffService',ShoppingListCheckOffService)
-ToBuyController.$inject=['ShoppingListCheckOffService'];
+//
 
-
-var TobuyList=[
+var listitem=[
 {
-  name:"Bread"
-  quantity:"12"
+  name:"Bread",
+  quantity:12
 },
 {
-  name:"Cookie"
-  quantity:"10"
+  name:"Cookie",
+  quantity:10
 },
 {
-  name:"Biscuit"
-  quantity:"5"
+  name:"Biscuit",
+  quantity:5
 },
 {
-  name:"Bread"
-  quantity:"12"
+  name:"Banana",
+  quantity:12
 },
 {
-  name:"Bread"
-  quantity:"12"
+  name:"Eggs",
+  quantity:12
 },
 
 ]
-function ToBuyController(ShoppingListCheckOffService,$scope){
-$scope.TobuyList=TobuyList;
 
+function ToBuyController(ShoppingListCheckOffService,$scope){
+//$scope.TobuyList=[];
+//$scope.TobuyList=listitem;
+var itemadder=this;
+itemadder.TobuyList=[];
+itemadder.TobuyList=listitem;
+itemadder.AddItem=function(item){
+ShoppingListCheckOffService.AddItem(item.name,item.quantity);
+itemadder.TobuyList=ShoppingListCheckOffService.removeItems(item);
+}
+}
+
+function AlreadyBoughtController(ShoppingListCheckOffService,$scope){
+//$scope.TobuyList=TobuyList;
+var showlist=this;
+showlist.items=ShoppingListCheckOffService.getItems();
+
+
+}
+
+function ShoppingListCheckOffService()
+{
+var service=this;
+var items=[];
+service.AddItem=function(itemname,quantity)
+{
+  var item={
+    name:itemname,
+    quantity:quantity
+  };
+  items.push(item);
+}
+service.getItems=function()
+{
+  return items;
+}
+service.removeItems=function(itemindex)
+{
+  listitem.splice(listitem.indexOf(itemindex),1);
+  return listitem;
+}
 }
 })();
